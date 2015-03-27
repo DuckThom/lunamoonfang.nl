b531a89a683176b2fbd7eb5136909cc67eb72786
Thomas Wiringa <DuckThom@users.noreply.github.com>
2015-03-27 19:12:23 +0100

    Added Perl to code-wookies project

--- app/views/home/projects.blade.php
+++ app/views/home/projects.blade.php
@@ -74,7 +74,7 @@
                                                 <table class="table table-condensed">
                                                         <tr>
                                                                 <td><strong><i class="fa fa-file-code-o"></i> Language</strong></td>
-                                                                <td>Bash</td>
+                                                                <td>Bash, Perl</td>
                                                         </tr>
                                                         <tr>
                                                                 <td><strong><i class="fa fa-clock-o"></i> Created</strong></td>
