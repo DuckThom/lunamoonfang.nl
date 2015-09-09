ce8b8a964369b06d6b5796580ad1e5f1d32989df
Thomas Wiringa <thomas.wiringa@gmail.com>
2015-09-09 22:40:12 +0200

    Added JS to submit form stuff

--- app/views/home/login.blade.php
+++ app/views/home/login.blade.php
@@ -7,17 +7,17 @@
 @section('content')
         <div class="row">
         	<div class="col-md-8 col-md-offset-2">
-        		<form action="" method="POST" class="form-horizontal">
+        		<form action="" method="POST" class="form-horizontal" id="loginForm">
                                 {{ Form::token() }}
 
-                                <paper-input name="username" label="Username"></paper-input>
+                                <paper-input name="username" label="Username" required></paper-input>
                                 <!--input type="text" name="username" class="form-control" id="inputUsername" placeholder="Username" {{{ (Session::has('username') ? 'value=' . Session::get('username') : '') }}}-->
-                                <paper-input type="password" name="password" label="Password"></paper-input>
+                                <paper-input type="password" name="password" label="Password" required></paper-input>
                                 <!--input type="password" name="password" class="form-control" id="inputPassword" placeholder="Password"-->
 
                                 <br />
 
-                                <paper-button id="loginButton" type="submit" raised>Sign in</paper-button>
+                                <paper-button id="loginButton" type="submit" raised on-click="submitLogin()">Sign in</paper-button>
                                 <!--button type="submit" class="btn btn-primary btn-block">Sign in</button-->
                         </form>
         	</div>
@@ -34,4 +34,12 @@
                         }
                 }
         </style>
+@stop
+
+@section('extraJS')
+        <script>
+                $('#loginButton').click(function() {
+                        $('#loginForm').submit();
+                });
+        </script>
 @stop
