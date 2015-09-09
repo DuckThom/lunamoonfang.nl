f3841eabd17d6b31c88a98a2f2af7aa9623e7665
Luna Moonfang <thomas.wiringa@gmail.com>
2015-09-09 21:13:08 +0200

    Changed controller comment

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -4,14 +4,11 @@ class HomeController extends BaseController {
 
 	/*
 	|--------------------------------------------------------------------------
-	| Default Home Controller
+	| Home Controller
 	|--------------------------------------------------------------------------
 	|
-	| You may wish to use controllers instead of, or in addition to, Closure
-	| based routes. That's great! Here is an example controller method to
-	| get you started. To route to this controller, just add the route:
-	|
-	|	Route::get('/', 'HomeController@showWelcome');
+	| This controller will handle all the basic pages like
+	| Home, Music and About me
 	|
 	*/
 
