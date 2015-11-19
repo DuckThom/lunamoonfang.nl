87aeb4e66068e6951462443e98e7f8f8f9383dcc
Luna Moonfang <thomas.wiringa@gmail.com>
2015-11-19 17:48:15 +0100

    undo the annoying https shit

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,51 +11,49 @@
 |
 */
 
-Route::group(['middleware' => 'https'], function() {
-    /**
-     * Image viewing
-     */
-     Route::group(['prefix' => 's'], function () {
-         Route::get('list', ['middleware' => 'auth', 'uses' => 'FileController@imagelist']);
-         Route::get('overview', ['middleware' => 'auth', 'uses' => 'FileController@imageoverview']);
-         Route::get('{image_name}', 'ImageController@showImage');
-         Route::get('{image_name}/full', 'ImageController@showFullImage');
-    });
-
-    /**
-     * File and image uploading
-     */
-    Route::group(['prefix' => 'u', 'middleware' => 'auth'], function () {
-        Route::post('image', 'FileController@saveImage');
-        Route::post('file', 'FileController@saveFile');
-    });
-
-    /**
-     * File and image uploading
-     */
-    Route::group(['prefix' => 'f'], function () {
-        Route::get('list', ['middleware' => 'auth', 'uses' => 'FileController@filelist']);
-        Route::get('{version}/{name}', 'FileController@serve');
-    });
-
-
-    /**
-     * The main site routes
-     */
-    Route::get('/', 'HomeController@index');
-    Route::get('music', 'HomeController@music');
-    Route::get('info', 'HomeController@info');
-    Route::get('projects', 'HomeController@projects');
-    Route::get('licenses', 'HomeController@licenses');
-    Route::get('clock', 'HomeController@clock');
-    Route::get('social', 'HomeController@social');
-
-    Route::get('upload', ['middleware' => 'auth', 'uses' => 'FileController@index']);
-
-    /**
-     * User related routes
-     */
-    Route::get('logout', ['middleware' => 'auth', 'uses' => 'UserController@logout']);
-
-    Route::match(['GET', 'POST'], '/login', 'UserController@login');
+/**
+ * Image viewing
+ */
+ Route::group(['prefix' => 's'], function () {
+     Route::get('list', ['middleware' => 'auth', 'uses' => 'FileController@imagelist']);
+     Route::get('overview', ['middleware' => 'auth', 'uses' => 'FileController@imageoverview']);
+     Route::get('{image_name}', 'ImageController@showImage');
+     Route::get('{image_name}/full', 'ImageController@showFullImage');
 });
+
+/**
+ * File and image uploading
+ */
+Route::group(['prefix' => 'u', 'middleware' => 'auth'], function () {
+    Route::post('image', 'FileController@saveImage');
+    Route::post('file', 'FileController@saveFile');
+});
+
+/**
+ * File and image uploading
+ */
+Route::group(['prefix' => 'f'], function () {
+    Route::get('list', ['middleware' => 'auth', 'uses' => 'FileController@filelist']);
+    Route::get('{version}/{name}', 'FileController@serve');
+});
+
+
+/**
+ * The main site routes
+ */
+Route::get('/', 'HomeController@index');
+Route::get('music', 'HomeController@music');
+Route::get('info', 'HomeController@info');
+Route::get('projects', 'HomeController@projects');
+Route::get('licenses', 'HomeController@licenses');
+Route::get('clock', 'HomeController@clock');
+Route::get('social', 'HomeController@social');
+
+Route::get('upload', ['middleware' => 'auth', 'uses' => 'FileController@index']);
+
+/**
+ * User related routes
+ */
+Route::get('logout', ['middleware' => 'auth', 'uses' => 'UserController@logout']);
+
+Route::match(['GET', 'POST'], '/login', 'UserController@login');
