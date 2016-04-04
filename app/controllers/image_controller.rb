8f4ff1dc59c5673653adfa7f07f3e76c7cca13d9
Thomas Wiringa <thomas.wiringa@gmail.com>
2016-04-04 16:09:38 +0200

    Save file mime type instead of extension

--- app/controllers/image_controller.rb
+++ app/controllers/image_controller.rb
@@ -4,7 +4,7 @@ class ImageController < ApplicationController
     image = Image.find_by_url_hash(params[:id]) or not_found
 
     if not image.nil?
-      response.header['Content-Type'] = "image/#{image.image_type}"
+      response.header['Content-Type'] = "#{image.image_type}"
       send_data image.image_data, type: image.image_type, disposition: 'inline'
     end
   end
--- app/models/image.rb
+++ app/models/image.rb
@@ -4,8 +4,7 @@ class Image < ActiveRecord::Base
   def self.save_file(upload, hash)
     file_name = upload['file'].original_filename if (upload['file'] != '')
     file_data = upload['file'].read
-
-    file_type = file_name.split('.').last
+    file_type = upload['file'].content_type
 
     self.create(name: file_name, url_hash: hash, image_type: file_type, image_data: file_data)
   end
