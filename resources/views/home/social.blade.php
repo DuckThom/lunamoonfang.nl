b535872554ae10c08f269f34bbf7fca2b6083a4a
Thomas Wiringa <thomas.wiringa@gmail.com>
2016-04-29 11:43:07 +0200

    scss: fix styling for social page

--- resources/views/home/social.blade.php
+++ resources/views/home/social.blade.php
@@ -1,59 +1,63 @@
-@extends('master')
-
-@section('title')
-	Social
-@stop
+@extends('master', ['title' => 'Social'])
 
 @section('content')
-        <div class="text-center">
-                <h3>My social profiles:</h3>
+    <div class="container">
+        <h3 class="social-title">My social profiles:</h3>
 
-                <div class="panel panel-default">
-                        <div class="panel-body">
-                                <div class="project-header">
-                                        <h3>Google+: (<a href="https://plus.google.com/+ThomasWiringa" target="_blank">+ThomasWiringa</a>)</h3>
-                                </div>
-                        </div>
+        <div class="social-blocks">
+            <div class="social google">
+                <div class="social-logo">
+                    <i class="fa fa-google-plus"></i>
                 </div>
+                <div class="social-link">
+                    <a href="https://plus.google.com/+ThomasWiringa" target="_blank">+ThomasWiringa</a>
+                </div>
+            </div>
 
-                <div class="panel panel-default">
-                        <div class="panel-body">
-                                <div class="project-header">
-                                        <h3>GitHub: (<a href="https://github.com/DuckThom" target="_blank">DuckThom</a>)</h3>
-                                </div>
-                        </div>
+            <div class="social github">
+                <div class="social-logo">
+                    <i class="fa fa-github"></i>
                 </div>
+                <div class="social-link">
+                    <a href="https://github.com/DuckThom" target="_blank">DuckThom</a>
+                </div>
+            </div>
 
-                <div class="panel panel-default">
-                        <div class="panel-body">
-                                <div class="project-header">
-                                        <h3>Twitter: (<a href="https://twitter.com/real_duckthom" target="_blank">@Real_DuckThom</a>)</h3>
-                                </div>
-                        </div>
+            <div class="social twitter">
+                <div class="social-logo">
+                    <i class="fa fa-twitter"></i>
                 </div>
+                <div class="social-link">
+                    <a href="https://twitter.com/real_duckthom" target="_blank">@Real_DuckThom</a>
+                </div>
+            </div>
 
-                <div class="panel panel-default">
-                        <div class="panel-body">
-                                <div class="project-header">
-                                        <h3>LastFM: (<a href="https://last.fm/user/DuckThom" target="_blank">DuckThom</a>)</h3>
-                                </div>
-                        </div>
+            <div class="social lastfm">
+                <div class="social-logo">
+                    <i class="fa fa-lastfm"></i>
                 </div>
+                <div class="social-link">
+                    <a href="https://last.fm/user/DuckThom" target="_blank">DuckThom</a>
+                </div>
+            </div>
 
-                <div class="panel panel-default">
-                        <div class="panel-body">
-                                <div class="project-header">
-                                        <h3>Steam: (<a href="https://steamcommunity.com/id/Luna_Moonfang" target="_blank">Luna_Moonfang</a>)</h3>
-                                </div>
-                        </div>
+            <div class="social steam">
+                <div class="social-logo">
+                    <i class="fa fa-steam"></i>
                 </div>
+                <div class="social-link">
+                    <a href="https://steamcommunity.com/id/Luna_Moonfang" target="_blank">Luna_Moonfang</a>
+                </div>
+            </div>
 
-                <div class="panel panel-default">
-                        <div class="panel-body">
-                                <div class="project-header">
-                                        <h3>YouTube: (<a href="https://youtube.com/user/DuckThom" target="_blank">DuckThom</a>)</h3>
-                                </div>
-                        </div>
+            <div class="social youtube">
+                <div class="social-logo">
+                    <i class="fa fa-youtube"></i>
                 </div>
+                <div class="social-link">
+                    <a href="https://youtube.com/user/DuckThom" target="_blank">DuckThom</a>
+                </div>
+            </div>
         </div>
+    </div>
 @stop
