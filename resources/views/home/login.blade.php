b7f534b62880b0d3f63d1c25c27966a53d8e2076
Thomas Wiringa <thomas.wiringa@gmail.com>
2016-05-02 13:42:06 +0200

    html: remove ids from form inputs

--- resources/views/home/login.blade.php
+++ resources/views/home/login.blade.php
@@ -3,16 +3,16 @@
 @section('content')
     <div class="container">
         <div class="column-md-1">
-            <form action="" method="POST" class="form" id="loginForm">
+            <form action="" method="POST" class="form">
                 {!! csrf_field() !!}
 
-                <label>Login</label>
-                <input type="text" name="username" class="form-control" id="inputUsername" placeholder="Username" value="{{ (Session::has('username') ? Session::get('username') : '') }}">
+                <label for="username">Login</label>
+                <input type="text" name="username" class="form-control" placeholder="Username" value="{{ old('username') }}">
 
                 <br />
 
-                <label>Password</label>
-                <input type="password" name="password" class="form-control" id="inputPassword" placeholder="Password">
+                <label for="password">Password</label>
+                <input type="password" name="password" class="form-control" placeholder="Password">
 
                 <button type="submit" class="button btn-success btn-block">Sign in</button>
             </form>
