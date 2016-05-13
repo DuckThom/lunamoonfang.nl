36f01de3a223200fc424942a54948142c0a67f25
Thomas Wiringa <thomas.wiringa@gmail.com>
2016-05-13 23:19:14 +0200

    Change {} locations

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -46,16 +46,12 @@ class Handler extends ExceptionHandler
             $e = new NotFoundHttpException($e->getMessage(), $e);
         }
 
-        if (config('app.debug'))
-        {
+        if (config('app.debug')) {
             $whoops = new \Whoops\Run;
 
-            if ($request->ajax())
-            {
+            if ($request->ajax()) {
                 $whoops->pushHandler(new \Whoops\Handler\JsonResponseHandler);
-            }
-            else
-            {
+            } else {
                 $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
             }
 
