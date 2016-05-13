a1c5dc5dd104f2c539eb089cd3f223c659bc35dc
Thomas Wiringa <thomas.wiringa@gmail.com>
2016-05-13 23:24:28 +0200

    Add BSOD-like 404 page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -2,41 +2,64 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>lunamoonfang.nl | Error 404</title>
+    <title>lunamoonfang.nl | 404 Not Found</title>
     <style>
-        @import url(//fonts.googleapis.com/css?family=Titillium+Web:700);
+        @import url(https://fonts.googleapis.com/css?family=VT323);
 
         body {
             margin:0;
-            font-family:'Titillium Web', sans-serif;
+            font-family: 'VT323';
             text-align:center;
-            color: #999;
+            color: lightgrey;
+            background-color: blue;
+            border-sizing: border-box;
+            -moz-border-sizing: border-box;
+            -webkit-border-sizing: border-box;
         }
 
         .welcome {
-            width: 100%;
-            position: absolute;
-            top: 40%;
+            max-width: 750px;
+            margin: 0 auto;
+            position: relative;
+            top: 100px;
             text-align: center;
+            font-size: 22px;
         }
 
         a, a:visited {
             text-decoration:none;
+            color: lightgray;
         }
 
         h1 {
+            display: inline-block;
+            background-color: lightgrey;
+            color: blue;
             font-size: 32px;
-            margin: 16px 0 0 0;
+            padding: 3px 6px;
+        }
+
+        .text, ul {
+            text-align: left;
         }
     </style>
 </head>
 <body>
 <div class="welcome">
     <h1>
-        404 Page not found
-        <br />
-        Please check your URL for errors
+        STOP
     </h1>
+    <p class="text">
+        A fatal exception 404 has ocurred at 0x{{ dechex(mt_rand(1048577, 16777216)) }} in {{ request()->server('HTTP_USER_AGENT') }}.
+        The current request will be terminated.
+    </p>
+
+    <ul>
+        <li><a href="">Press here</a> to go back to the previous page</li>
+        <li>Press any other key to do probably nothing</li>
+    </ul>
+
+    <p style="text-align: center;">Press any key to continue _</p>
 </div>
 </body>
 </html>
