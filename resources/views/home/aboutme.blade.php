30f60a02bbb732a89fbf6f4c5f4f52fe10fa9547
Thomas Wiringa <thomas.wiringa@gmail.com>
2016-08-23 13:50:42 +0200

    Change about me text

--- resources/views/home/aboutme.blade.php
+++ resources/views/home/aboutme.blade.php
@@ -10,23 +10,24 @@
             <div class="text-center">
                 <p class="text-block">
                     Hi! My name is Thomas Wiringa but on the interwebs my nickname is usually DuckThom or Luna Moonfang.<br />
-                    I'm {{ date_diff(date_create(date('Y-n-j')), date_create("1995-11-26"))->format('%y') }} years old and live in The Netherlands. <br />
+                    I'm {{ date_diff(date_create(date('Y-n-j')), date_create("1995-11-26"))->format('%y') }} years old and live in The Netherlands.<br />
+                    My native language is Dutch but on the internet I mostly speak/write in English. <br />
+                    I am also learning Swedish in my spare time as I'm planning to move to Sweden in the future.<br />
                     <br />
-                    <br />
-                    My favorite programming languages are PHP, HTML5, JS (jQuery and a little bit of NodeJS) and CSS3 (LESS/SASS) but I can also code in Java, C#, Swift, Ruby and Python<br />
+                    My favourite programming languages are PHP (Primarily Laravel), HTML5, JS and CSS3 but I can also code in Java, C#, Swift, Ruby and Python.<br />
                     I'm currently learning more about Ruby and NodeJS (React, Meteor).<br />
                     I recently graduated from school and I can now call myself a "Software Engineer".<br />
                     <br />
                     I also play games quite a lot. <br />
-                    The games I've been playing the most lately are Rocket League and GTA V but I also play games like Hearthstone, Halo, DOTA and Diablo.<br />
+                    The games I've been playing the most lately are Rocket League and GTA V but I also play games like Overwatch, Hearthstone, Halo, DOTA and Diablo.<br />
                     For a more complete list of games, take a look at my Steam profile: <a href="https://steamcommunity.com/id/Luna_Moonfang" target="_blank">[HatScrub] Luna Moonfang</a>.<br />
                     <br />
                     For as far as music goes, I am a huge fan of <a href="http://monstercat.com">Monstercat</a> and Muse.<br />
-                    My favorite radio station is <a href="http://3fm.nl" target="_blank">3FM</a> on the radio and <a href="https://twitch.tv/monstercat" target="_blank">Monstercat FM</a> on Twitch.<br />
-                    Electronic music is my favourite kind of music.<br />
+                    My favourite radio station is <a href="http://3fm.nl" target="_blank">3FM</a> on the radio and <a href="https://twitch.tv/monstercat" target="_blank">Monstercat FM</a> on Twitch.<br />
+                    Electronic music is my favourite genre.<br />
                     <br />
                     Which brings me to the next point, YouTube. <br />
-                    I'm subscribed to a lot of channels but <a href="https://youtube.com/hatfilms" target="_blank">Hat Films</a>, <a href="https://youtube.com/yogscastsips" target="_blank">Sips</a> and <a href="https://youtube.com/yogscast" target="_blank">The Yogscast</a> are my favorites.<br />
+                    I'm subscribed to a lot of channels but <a href="https://youtube.com/hatfilms" target="_blank">Hat Films</a>, <a href="https://youtube.com/yogscastsips" target="_blank">Sips</a> and <a href="https://youtube.com/yogscast" target="_blank">The Yogscast</a> are my favourites.<br />
                     <br />
                 </p>
             </div>
@@ -40,7 +41,7 @@
             <h3 class="content-header">Contact</h3>
 
             <p class="text-center">
-                I prefer correspondation via Twitter but i'm also available via IRC and Google+.
+                I prefer to correspond via Twitter or E-Mail but i'm also available via IRC and Google+.
             </p>
 
             <table class="table table-striped">
